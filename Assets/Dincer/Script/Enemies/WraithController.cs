19ce3345f3471e8fee7674f4efae2a9d478f3eed
DinçerÖğren <emre.ogren@ogr.sakarya.edu.tr>
2024-12-02 02:56:18 +0300

    6

--- Assets/Dincer/Script/Enemies/WraithController.cs
+++ Assets/Dincer/Script/Enemies/WraithController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Analytics;
 using static UnityEngine.GraphicsBuffer;
 
 public class WraithController : MonoBehaviour
@@ -13,26 +14,43 @@ public class WraithController : MonoBehaviour
     public float moveSpeed;
     public LayerMask playerLayer;
     bool startedWalking;
+    bool walkDone;
 
     [Header("Throw Skil")]
     public GameObject projPrefab;
     public float projSpeed;
     public float timeBetweenProjectiles;
     public float maxProjAmount;
-    float projTimer;
+    float projTimer = Mathf.Infinity;
     public float projectileSkillRange;
     public float projLifetime;
     public float projDamage;
+    public bool shouldStartProjectileThrow;
+    public Transform projectileExitPoint;
+    public float playerHeightCorrection = 1f;
     //add projectiles script for impact effects;
 
     [Header("Meteor Shower Skill")]
-    public GameObject meteorPrefab;
+    public bool useMeteor;
+    public Meteor meteorPrefab;
+
+    public float meteorFallSpeed;
     //add meteors script for impact efect
     public float maxMeteorHeight;
     public float meteorAmount;
     //Randomize meteor drops karakterin etrafýna,
     public float meteorDamage;
+    public float randomOffsetMultiplier;
+    public float meteorAirWaitTime = 1f;
 
+    [Header("Meteor Ascend Settings")]
+    public float ascendHeight;
+    public float ascendForce;
+    public float ascendDuration = 15f;
+    float ascendTimer = Mathf.Infinity;
+    bool isAscending;
+    public Transform groundCheckPos;
+    public float groundCheckDist;
 
     [Header("Shield and Spawn")]
     public float shieldDuration;
@@ -57,14 +75,14 @@ public class WraithController : MonoBehaviour
 
     private void Awake()
     {
-        rb=GetComponent<Rigidbody>();
-        anim=GetComponent<Animator>();
-        player=GameObject.FindWithTag("Player").transform;
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        player = GameObject.FindWithTag("Player").transform;
     }
 
     private void Start()
     {
-        
+
     }
 
 
@@ -72,7 +90,7 @@ public class WraithController : MonoBehaviour
     {
 
         UpdateAnimator();
-
+        CheckGrounded();
         if (startedWalking)
         {
             transform.LookAt(player);
@@ -82,20 +100,34 @@ public class WraithController : MonoBehaviour
         {
             startedWalking = true;
             StartCoroutine(WalkTowardsPlayer());
+
+        }
+
+
+        if (walkDone)
+        {
+            if (shouldStartProjectileThrow)
+            {
+                shouldStartProjectileThrow = false;
+                StartProjectileThrow();
+            }
+            if (useMeteor)
+            {
+                useMeteor = false;
+                StartMeteorSkill();
+            }
         }
 
     }
 
 
-    void UpdateAnimator()
-    {
-        anim.SetFloat("speed", rb.velocity.magnitude);
-    }
+
+    #region WalkSection
     private IEnumerator WalkTowardsPlayer()
     {
-        float movTime = 0;   
+        float movTime = 0;
 
-        while (moveTime>movTime)
+        while (moveTime > movTime)
         {
             print("In while " + movTime);
             Vector3 dir = (player.position - transform.position).normalized;
@@ -103,13 +135,188 @@ public class WraithController : MonoBehaviour
             movTime += Time.deltaTime;
             yield return null;
         }
+        walkDone = true;
         print("Move Ended");
     }
 
     bool IsInActivateRange() => Physics.CheckSphere(transform.position, activateRange, playerLayer);
 
+    #endregion
+
+    #region ProjectileSection
+
+    void StartProjectileThrow()
+    {
+        if (IsPlayerInRange(projectileSkillRange))
+        {
+            anim.SetTrigger("Throw");
+        }
+        else
+            MoveTowardsPlayer();
+    }
+
+    public void Throw()
+    {
+        StartCoroutine(ProjectileThrow());
+    }
+
+    IEnumerator ProjectileThrow()
+    {
+        float a = 0;
+
+        while (a <= maxProjAmount)
+        {
+            if (projTimer > timeBetweenProjectiles)
+            {
+                projTimer = 0;
+                a++;
+                SpawnProjectile();
+            }
+
+            projTimer += Time.deltaTime;
+            yield return null;
+        }
+
+        shouldStartProjectileThrow = false;
+        anim.ResetTrigger("Throw");
+    }
+
+    void SpawnProjectile()
+    {
+        var a = Instantiate(projPrefab, projectileExitPoint.position, Quaternion.identity);
+        Vector3 playerPos = player.position;
+        playerPos.y -= playerHeightCorrection;
+        Vector3 dir = (playerPos - transform.position).normalized;
+        print("Dir = " + dir);
+        //dir.y -= playerHeightCorrection;
+        print("Dir after correction = " + dir);
+
+        a.GetComponent<Rigidbody>().velocity = dir * projSpeed;
+        //a.lifetime = lifetime;
+
+    }
+
+
+
+
+    #endregion
+
+
+    #region Meteor Section
+
+    void StartMeteorSkill()
+    {
+        if (IsPlayerInRange(projectileSkillRange))
+        {
+            //anim.SetTrigger("Meteor");
+            StartCoroutine(AscendPlayer());
+        }
+        else
+            MoveTowardsPlayer();
+    }
+
+
+
+    IEnumerator AscendPlayer()
+    {
+        rb.velocity = Vector3.zero;
+        ascendTimer = 0;
+        while (ascendTimer<ascendDuration)
+        {
+
+            if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitInfo, 100, LayerMask.GetMask("Ground")))
+            {
+                float height = hitInfo.point.y + ascendHeight;
+
+
+                float dist = (height - transform.position.y);
+                float amountToLift;
+                amountToLift = dist * ascendForce - GetPlayerVerticalVelocity().y;
+                Vector3 liftForce = new(0, amountToLift, 0);
+                rb.AddForce(liftForce, ForceMode.VelocityChange);
+                ascendTimer += Time.deltaTime;
+
+            }
+            yield return null;
+        }
+
+        print("Ascend ended");
+    }
+
+
+    public void TriggerMeteorShower() { StartCoroutine(MeteorShower()); }
+    IEnumerator MeteorShower()
+    {
+        Debug.Log("Meteor Shower Activated!");
+
+        for (int i = 0; i < meteorAmount; i++)
+        {
+            
+
+            // Spawn meteor
+            SpawnMeteor();
+            yield return null;
+        
+        }
+    }
+
+    private void SpawnMeteor()
+    {
+        Vector3 randomOffset = UnityEngine.Random.insideUnitSphere * randomOffsetMultiplier;
+        randomOffset.y = maxMeteorHeight;
+        Vector3 spawnPosition = player.position + randomOffset;
+
+
+        var a = Instantiate(meteorPrefab, spawnPosition, Quaternion.identity);
+
+        a.SetWaitSpeed(meteorAirWaitTime);
+        a.SetFallSpeed(meteorFallSpeed);
+
+    }
+
+
+
+
+
+
+
+    #endregion
+
+
+
+    private Vector3 GetPlayerVerticalVelocity()
+    {
+        return new Vector3(0, rb.velocity.y, 0);
+    }
+    private void MoveTowardsPlayer()
+    {
+        throw new NotImplementedException();
+    }
+    bool IsPlayerInRange(float range)
+    {
+        return Vector3.Distance(player.position, transform.position) < range;
+
+    }
+
+    void CheckGrounded()
+    {
+        isAscending = !(Physics.Raycast(groundCheckPos.position, Vector3.down,groundCheckDist, LayerMask.GetMask("Ground")));
+    }
+    void UpdateAnimator()
+    {
+        anim.SetFloat("speed", rb.velocity.magnitude);
+        anim.SetBool("isAscending", isAscending);
+    }
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, activateRange);
+
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawWireSphere(transform.position, projectileSkillRange);
+
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawRay(groundCheckPos.position, Vector3.down * groundCheckDist);
     }
 }
---
+++ Assets/Dincer/Script/Meteor.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+    public float meteorFallSpeed;
+    public float indicatorYOffset;
+
+    float meteorFallWait=2f;
+    float fallTimer=0;
+
+    Transform indicator;
+
+    Rigidbody rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        indicator=transform.GetChild(0);
+        indicator.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (fallTimer<meteorFallWait)
+        {
+            fallTimer += Time.deltaTime;
+            return;
+        }
+        Descend();
+        CreateIndicator();
+    }
+
+    private void CreateIndicator()
+    {
+        if(Physics.Raycast(transform.position,Vector3.down,out RaycastHit hit, 100, LayerMask.GetMask("Ground")))
+        {
+
+            indicator.position = hit.point + Vector3.up * indicatorYOffset;
+            indicator.gameObject.SetActive(true);
+        }
+    }
+
+    void Descend()
+    {
+        rb.velocity = Vector3.down * meteorFallSpeed;
+    }
+
+    public void SetWaitSpeed(float waitSpeed)
+    {
+        meteorFallWait = waitSpeed;
+    }
+    public void SetFallSpeed(float fallSpeed)
+    {
+        meteorFallSpeed = fallSpeed;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.collider.CompareTag("Ground"))
+        {
+            gameObject.SetActive(false);
+        }
+    
+    }
+}
